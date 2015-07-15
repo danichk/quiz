b85ac37567a52ad2ba04a5363bab4b34e5799c73
Daniel Checa Martínez <dani89.cm@gmail.com>
2015-07-15 18:48:22 +0200

    Editar pregunta

--- controllers/author_controller.js
+++ controllers/author_controller.js
@@ -1,4 +1,4 @@
 // GET /author
 exports.credits = function (req, res) {
-	res.render('author', {name: 'Dani Checa', avatar: '/images/avatar.png', video: ''});
+	res.render('author', {name: 'Dani Checa', avatar: '/images/avatar.png', video: '', errors: []});
 };
--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -78,3 +78,27 @@ exports.create = function(req, res) {
       	}
     });
 };
+
+// GET /quizes/:id/edit
+exports.edit = function (req, res) {
+	var quiz = req.quiz; // autoload de instancia de quiz
+
+	res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+// PUT /quizes/:id
+exports.update = function (req, res) {
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+
+	req.quiz.validate().then(function (err) {
+		if (err) {
+			res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+		}
+		else {
+			req.quiz.save({fields: ["pregunta", "respuesta"]}).then(function () {
+				res.redirect('/quizes');
+			});
+		}
+	});
+};
