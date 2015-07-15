a73fd2bb5927aa153e9a37d2015174c3c2e5b406
Daniel Checa Martínez <dani89.cm@gmail.com>
2015-07-15 19:51:09 +0200

    Borrar pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -102,3 +102,12 @@ exports.update = function (req, res) {
 		}
 	});
 };
+
+// DELETE /quizes/:id
+exports.destroy = function (req, res) {
+	req.quiz.destroy().then(function () {
+		res.redirect('/quizes');
+	}).catch(function (error) {
+		next(error);
+	});
+};
--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,7 @@ router.get('/quizes/new', quizController.new);
 router.post('/quizes/create', quizController.create);
 router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
 router.put('/quizes/:quizId(\\d+)', quizController.update);
+router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
 
 router.get('/author', authorController.credits);
 
